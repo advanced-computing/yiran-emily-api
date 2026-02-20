682df3d54c00b5f31cb8d560f226a91e0531c242
Yiran Ge <yg3102@columbia.edu>
2026-02-20 14:55:23 -0500

    Delete test_helper.py

--- test_helper.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-def parse_dollars(amounts: pd.Series):
-    dollar_str = amounts.str.replace("$", '', regex=False).str.replace(',' , '').astype(float)
-    return dollar_str.astype(float)
